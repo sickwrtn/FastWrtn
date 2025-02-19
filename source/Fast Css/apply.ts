254007d811d18916fdd1dac4dbdf1b977f1f3324
sickwrtn <sillo154265@gmail.com>
2025-02-20 00:42:59 +0900

    Fix Out Css 문제

--- source/Fast Css/apply.ts
+++ source/Fast Css/apply.ts
@@ -1,22 +1,39 @@
 import * as env from "../.env/env";
+import { debug } from "../tools/debug";
 import { compileCss } from "./compiler";
 
 export function cssApply (){
-    if (JSON.parse(localStorage.getItem(env.local_them)).css != null && JSON.parse(localStorage.getItem(env.local_them)).css != ""){
-        var cssInitApply = document.createElement('style');
-        cssInitApply.innerHTML = `
-            .css-1ah7q4v {
-            background : transparent;
+    setInterval(()=>{
+        if (document.URL.includes('/character/u/')){
+            if (document.getElementById('ThemCssInit') == null && document.getElementById('ThemCss') == null){
+                if (JSON.parse(localStorage.getItem(env.local_them)).css != null && JSON.parse(localStorage.getItem(env.local_them)).css != ""){
+                    var cssInitApply = document.createElement('style');
+                    cssInitApply.setAttribute('id','ThemCssInit');
+                    cssInitApply.innerHTML = `
+                        .css-1ah7q4v {
+                        background : transparent;
+                        }
+                        .css-1e3ks9w {
+                        background : transparent;
+                        }
+                        .css-dorp2h {
+                        background : transparent;
+                        }`;
+                    var cssApply = document.createElement('style');
+                    cssApply.setAttribute('id','ThemCss');
+                    cssApply.innerHTML = compileCss(JSON.parse(localStorage.getItem(env.local_them)).css.split("\n").join(""));
+                    document.head.appendChild(cssInitApply);
+                    document.head.appendChild(cssApply);
+                    debug("insert ThemCss");
+                }
             }
-            .css-1e3ks9w {
-            background : transparent;
+        }
+        else{
+            if (document.getElementById('ThemCssInit') != null && document.getElementById('ThemCss') != null){
+                document.getElementById('ThemCssInit').remove();
+                document.getElementById('ThemCss').remove();
+                debug("Remove ThemCss");
             }
-            .css-dorp2h {
-            background : transparent;
-            }`;
-        var cssApply = document.createElement('style');
-        cssApply.innerHTML = compileCss(JSON.parse(localStorage.getItem(env.local_them)).css.split("\n").join(""));
-        document.head.appendChild(cssInitApply);
-        document.head.appendChild(cssApply);
-    }
+        }
+    })
 }
